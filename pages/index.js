61224ce04b6bf7913e33de218d52aa90352a1f14
Erick Rosa <erickpmotta@gmail.com>
2022-01-24 16:11:06 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+//Componente React
+function HomePage() {
+  //JSX
+  return (
+    <div>
+      <h1>Boas vindas</h1>
+      <h2>Discord</h2>
+
+      <style jsx>
+        {`
+          h1 {
+            color: #cece;
+          }
+        `}
+      </style>
+    </div>
+  );
+}
+
+export default HomePage;
